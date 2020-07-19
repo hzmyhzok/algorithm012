12b21fd2e50a2e4104626fc62569f8416778ba51
hz <hzmyhzok@163.com>
2020-07-19 18:41:32 +0800

    week01 assignment ver3.0

---
+++ Week_01/88合并两个有序数组.py
@@ -0,0 +1,32 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        '''
+        利用双指针从后往前填充数组，不需要开辟额外空间
+        '''
+        p1 = m-1
+        p2 = n-1
+        p = m+n-1
+        while p1 >= 0 and p2 >=0:
+            if nums1[p1] < nums2[p2]:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            else:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            p -= 1
+        nums1[:p2 + 1] = nums2[:p2 + 1]
+
+
+
+'''
+直接合并后排序
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[:] = sorted(nums1[:m] + nums2)
+'''
